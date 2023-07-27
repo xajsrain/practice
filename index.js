df0d5a6eb33f677ba6199e06d6c9a00bcf4976d1
xajsrain <1528067402@qq.com>
2023-07-28 00:13:04 +0800

    feat: study dotenv

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const dotenv = require('dotenv')
-dotenv.config();
+console.log('# start---------')
+const conf = dotenv.config({debug: true,override: true}).parsed;
+// console.log(conf);
+// console.log('---------')
+// const buf = Buffer.from('say=hello')
+// const bufRes = dotenv.parse(buf)
+// console.log(bufRes)
+// console.log('---------')
+// const buf = Buffer.from('hello world')
+// const opt = { debug: true }
+// const config = dotenv.parse(buf, opt)
+// console.log(config)
+console.log('---------')
+// const aa = { gd: 'jackli', acc: 111 };
+// const conf = dotenv.populate(process.env, aa)
 console.log(process.env)
+console.log('# end---------')
+
